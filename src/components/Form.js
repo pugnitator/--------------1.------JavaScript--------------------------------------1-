40765255695a5576c658f9a3a4d5439363d7716d
pugnitator <pugnitator2000@gmail.com>
2024-09-27 00:33:23 +0300

    add form and listItem elements

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,27 @@ export class Form extends Component {
     this.$rootElement = document.createElement('form');
     this.$rootElement.className = 'donate-form';
 
-    // ...
+    const label = document.createElement('label');
+    label.className = 'donate-form__input-label';
+    label.textContent = 'Введите сумму в $';
+
+    const input = document.createElement('input');
+    input.className = 'donate-form__donate-input';
+    input.name = 'amount';
+    input.type = 'number';
+    input.max = '100';
+    input.min = '1';
+    input.required = true;
+
+    const button = document.createElement('button');
+    button.className = 'donate-form__submit-button';
+    button.type = 'submit';
+    button.textContent = 'Задонатить';
+
+    label.append(input);
+    this.$rootElement.append(label);
+    this.$rootElement.append(button);
+
   }
 
   handleInput(event) {
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,11 @@ export class ListItem extends Component {
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'donate-item';
 
-    // ...
+    const donate = document.createElement('b');
+    donate.textContent = props.donate
+
+    this.$rootElement.textContent = `${props.date}, ${props.time} - ${props.donate.bold()}`;
   }
 }
+
+// В пропсе ожидаем объект, в который поместим инфу с датой и временем клика на донат + суммой доната 
